a38f726f565f0d6bbc4ade18a252b76b255a09ba
Fernando Avellar <favellar@yahoo.com.br>
2020-01-16 22:09:53 -0300

    After Head creation

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+/* class Header extends Component {
+    render() {
+        return  <header id="main-header">JSHunt</header>
+    }
+} assim ou da forma abaixo conhecida como stateless component*/
+
+const Header = () => <header id="main-header">JSHunt</header>
+
+export default Header;
